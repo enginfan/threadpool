e1b6f716c9ea74d3c8cfb71b1745a9eaa9d86cd1
fan <624824229@qq.com>
2023-02-11 10:43:22 +0800

    threadpool类的设计

--- threadpool.h
+++ threadpool.h
@@ -5,6 +5,8 @@
 #include <queue>
 #include <memory>
 #include <atomic>
+#include <mutex>
+#include <condition_variable>
 //任务抽象基类
 
 class Task {
@@ -14,7 +16,7 @@ public:
 };
 
 //线程池支持的模式
-enum PoolMode {
+enum class PoolMode {
 	MODE_FIXED,//固定数量的线程
 	MODE_CACHED,//线程数量可动态增长
 };
@@ -26,8 +28,14 @@ private:
 };
 
 //线程池类型
-class TreadPool {
+class ThreadPool {
 public:
+	ThreadPool();
+	~ThreadPool();
+
+	void setMode(PoolMode mode);
+
+	void start();//开启线程池
 private:
 	std::vector<Thread*> threads_;//线程列表
 	size_t initThreadSize_;//初始的线程数量
@@ -36,7 +44,11 @@ private:
 	std::atomic_uint taskSize_;//任务的数量
 	int taskQusMaxThreshHold_; // 任务队列数量上限阈值
 	
+	std::mutex taskQueMtx_;//保证任务队列线程安全
+	std::condition_variable notFull_;//表示任务队列的不满
+	std::condition_variable notEmpty_;//表示任务队列的不空
 
+	PoolMode poolMode_;//当前线程池的工作模式
 };
 
 
